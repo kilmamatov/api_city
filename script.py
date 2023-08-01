35395eb45cdd2e5f309d010732aee8afa89217bd
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-08-02 01:04:05 +0500

    #1

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import subprocess
 import database_filling
+import platform
+
 
 if __name__ == '__main__':
-    subprocess.run(['python3', '-m', 'venv', 'venv'])
-    subprocess.run(['sh', 'venv/bin/activate'])
+    if platform.system() == 'Windows':
+        subprocess.run(['python', '-m', 'venv', 'venv'])
+        subprocess.run(['venv\\Scripts\\activate.bat'])
+    else:
+        subprocess.run(['python3', '-m', 'venv', 'venv'])
+        subprocess.run(['sh', 'venv/bin/activate'])
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
     subprocess.run(['alembic', 'revision', '--autogenerate'])
     subprocess.run(['alembic', 'upgrade', 'head'])
