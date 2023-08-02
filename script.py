59f9680ec4ca1f68f0a1e4d1035d6680ecaeb78d
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-08-03 01:38:56 +0500

    #1

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 import subprocess
 
 if __name__ == '__main__':
-    # Создание виртуального окружения
     subprocess.run(['python3', '-m', 'venv', 'venv'])
 
-    # Установка пакетов внутри виртуального окружения
     subprocess.run(["venv/bin/pip", "install", "-r", 'requirements.txt'])
 
     subprocess.run(["venv/bin/python3", "-m", "alembic", "upgrade", 'head'])
 
-    from database_filling import fill_database
-    fill_database()
+    input_commands = 'from database_filling import fill_database\n' \
+                     'fill_database()\n'
 
-    subprocess.run(["venv/bin/python3", "-m", "uvicorn", "src.main:app", '--reload', '--host', '0.0.0.0', '--port', '80'])
+    subprocess.run('venv/bin/python3', input=input_commands, text=True, shell=True)
+
+    subprocess.run(["venv/bin/python3", "-m", "uvicorn", "src.main:app", '--reload', '--host', '127.0.0.1', '--port', '8000'])
 
 
