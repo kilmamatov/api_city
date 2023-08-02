ab508a6289b3426c959917c47663d8dc89544677
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-08-02 20:05:10 +0500

    #1

--- script.py
+++ script.py
@@ -1,34 +1,16 @@
 import subprocess
 from database_filling import fill_database
-import sys
-import platform
-from pathlib import Path
 
+# Создание виртуального окружения
+subprocess.run(['python3', '-m', 'venv', 'venv'])
 
-# venv_dir = Path(__file__).parent / 'venv'
-# python_executable = venv_dir / 'bin' / 'python'
-# subprocess.run([sys.executable, '-m', 'venv', str(venv_dir)])
-# subprocess.run([str(python_executable), '-m', 'pip', 'install', '-r', 'requirements.txt'])
-# subprocess.run(['alembic', 'revision', '--autogenerate'])
-# subprocess.run(['alembic', 'upgrade', 'head'])
-# database_filling.fill_database()
-# subprocess.run(['uvicorn', 'src.main:app', '--reload', '--host', '127.0.0.1', '--port', '8000'])
+# Установка пакетов внутри виртуального окружения
+subprocess.run(["venv/bin/pip", "install", "-r", 'requirements.txt'])
 
-if __name__ == '__main__':
+subprocess.run(["venv/bin/python3", "-m", "alembic", "upgrade", 'head'])
 
-    subprocess.run(['python3', '-m', 'venv', 'venv'], check=True)
+fill_database()
 
-    install_command = 'source venv/bin/activate ' \
-                      '&& pip install -r requirements.txt'
-    subprocess.run(install_command, shell=True, check=True)
+subprocess.run(["venv/bin/python3", "-m", "uvicorn", "src.main:app", '--reload', '--host', '0.0.0.0', '--port', '80'])
 
-    alembic_upgrade = 'source venv/bin/activate ' \
-                      '&& alembic upgrade head'
-    subprocess.run(alembic_upgrade, shell=True, check=True)
-
-    fill_database()
-
-    uvicorn = 'source venv/bin/activate ' \
-              '&& uvicorn src.main:app --reload --host 127.0.0.1 --port 8000'
-    subprocess.run(uvicorn, shell=True, check=True)
 
