d755ef2a3f24938827923f28f5784bb29b17e650
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-08-02 20:13:01 +0500

    #1

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
 import subprocess
-from database_filling import fill_database
 
-# Создание виртуального окружения
-subprocess.run(['python3', '-m', 'venv', 'venv'])
+if __name__ == '__main__':
+    # Создание виртуального окружения
+    subprocess.run(['python3', '-m', 'venv', 'venv'])
 
-# Установка пакетов внутри виртуального окружения
-subprocess.run(["venv/bin/pip", "install", "-r", 'requirements.txt'])
+    # Установка пакетов внутри виртуального окружения
+    subprocess.run(["venv/bin/pip", "install", "-r", 'requirements.txt'])
 
-subprocess.run(["venv/bin/python3", "-m", "alembic", "upgrade", 'head'])
+    subprocess.run(["venv/bin/python3", "-m", "alembic", "upgrade", 'head'])
 
-fill_database()
+    from database_filling import fill_database
+    fill_database()
 
-subprocess.run(["venv/bin/python3", "-m", "uvicorn", "src.main:app", '--reload', '--host', '0.0.0.0', '--port', '80'])
+    subprocess.run(["venv/bin/python3", "-m", "uvicorn", "src.main:app", '--reload', '--host', '0.0.0.0', '--port', '80'])
 
 
